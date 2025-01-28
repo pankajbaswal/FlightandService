c1175f3fbe68ad9b798ea9935167d022f0f94733
Pankaj Baswal <pankajbaswal3360@gmail.com>
2025-01-28 10:16:02 +0530

    body-parser added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const {PORT}=require("./config/serverconfig")
+const {PORT}=require("./config/serverconfig");
+const bodyparser=require("body-parser");
+
 const setupAndStartServer = async () => {
   // /create the express object
   const app = express();
+  app.use(bodyparser.json);
+  app.use(bodyparser.urlencoded({extended:true}));
   app.listen(PORT, () => {
     console.log(`server started at ${PORT}`);
-    console.log(process.env);
   });
 };
 setupAndStartServer;
